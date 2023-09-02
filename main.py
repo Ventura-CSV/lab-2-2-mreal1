2d21ce0c6d451dd064cd239f37c7711137c1a725
Kyuwoong Lee <kwooler@gmail.com>
2023-09-01 21:14:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-    workhours = (input('45'))
+    workhours = int(input('Enter working hours'))
     reg_hours = 40
     reg_rate = 18.25
     ov_rate = 27.78
@@ -7,25 +7,26 @@ def main():
    ##################################################
    # Code your program here
    ##################################################
-    # overtime = workhours - reg_hours
-    # overtime_wage = overtime * ov_rate
-    # regular_wage = reg_hours * reg_rate
-    # total_wage = regular_wage + overtime_wage
-overtime = 45 - 40
-overtime_wage = 45 * 27.78
-regular_wage = 40 *18.25
-total_wage = 18.25 + 27.18
+    overtime = workhours - reg_hours
+    overtime_wage = overtime * ov_rate
+    regular_wage = reg_hours * reg_rate
+    total_wage = regular_wage + overtime_wage
+# overtime = 45 - 40
+# overtime_wage = 45 * 27.78
+# regular_wage = 40 *18.25
+# total_wage = 18.25 + 27.18
 
 
 
-print(f"Regular hours: {40} Regular Charge: {18.25}")
-print(f"Overtime hours: {45} Overtime Charge:  {27.78:.2f}")
+# print(f"Regular hours: {40} Regular Charge: {18.25}")
+print(f"Regular hours: {40} Regular Charge: {reg_hours}")
+print(f"Overtime hours: {45} Overtime Charge:  {overtime:.2f}")
 print(f"Total wage : {total_wage:.2f}")
 
    ##################################################
    # Do not delete the return statement
    ##################################################
-"return regular_wage, overtime_wage, total_wage"
+return regular_wage, overtime_wage, total_wage
 
 if __name__ == '__main__':
     main()
